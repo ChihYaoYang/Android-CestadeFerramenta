7e0afd19b6ef661a55913e96120aa8ae19fa66ac
Marcos <marcos.camberto@aluno.sc.senac.br>
2019-07-24 22:01:09 -0300

    add textview result

--- app/src/main/java/com/example/senac/cestadeferramenta/Conversao.java
+++ app/src/main/java/com/example/senac/cestadeferramenta/Conversao.java
@@ -3,12 +3,20 @@ package com.example.senac.cestadeferramenta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Conversao extends AppCompatActivity {
-
+    EditText editValor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversao);
+
+        editValor = findViewById(R.id.editValor);
+    }
+
+    public void conversao(View V) {
+        String valor = editValor.getText().toString();
     }
 }
