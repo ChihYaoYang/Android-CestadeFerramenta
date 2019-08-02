a3713097ae39cf650cad7faacbda38fe6f901a7e
Marcos <marcos.camberto@aluno.sc.senac.br>
2019-08-01 21:07:30 -0300

    add show result in product

--- app/src/main/java/com/example/senac/cestadeferramenta/helper/AdapterList.java
+++ app/src/main/java/com/example/senac/cestadeferramenta/helper/AdapterList.java
@@ -1,9 +1,13 @@
 package com.example.senac.cestadeferramenta.helper;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.senac.cestadeferramenta.R;
@@ -40,9 +44,19 @@ public class AdapterList extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = activity.getLayoutInflater().inflate(R.layout.produto_item, parent, false);
         Produto produto = produtos.get(position);
+        //find id
+        ImageView imagem = view.findViewById(R.id.imagemProduto);
         TextView txNome = view.findViewById(R.id.nomeProduto);
+        TextView quantidade = view.findViewById(R.id.qtdProduto);
+        TextView stattus = view.findViewById(R.id.statusProduto);
+        //Converte IMG
+        byte[] decodedString = Base64.decode(produto.getFoto(), Base64.DEFAULT);
+        Bitmap decodeByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        //Exibir
+        imagem.setImageBitmap(decodeByte);
         txNome.setText(produto.getNome());
-
+        quantidade.setText("QTD: " + produto.getQuantidade());
+        stattus.setText(produto.getStatus());
         return view;
     }
 
